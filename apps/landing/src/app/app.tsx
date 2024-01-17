94f503e642223b50f1bd6adf84aca7b603083c55
andrmaz <60042277+andrmaz@users.noreply.github.com>
2024-01-17 23:04:50 +0100

    feat: query

--- apps/landing/src/app/app.tsx
+++ apps/landing/src/app/app.tsx
@@ -1,21 +1,7 @@
 import * as React from 'react'
-import {Accordion, Query} from '@wallet/shared-ui'
-import {useGetUsersQuery} from '../hooks/user'
 
 function App() {
-  const result = useGetUsersQuery()
-  return (
-    <Query result={result}>
-      {data => {
-        return (
-          <>
-            <Accordion />
-            {JSON.stringify(result.data?.user?.name, null, 2)}
-          </>
-        )
-      }}
-    </Query>
-  )
+  return <main></main>
 }
 
 export default App
--- apps/landing/src/hooks/user.ts
+++ apps/landing/src/hooks/user.ts
@@ -2,11 +2,10 @@ import { useQuery } from "@tanstack/react-query"
 import { graphql } from '@wallet/shared-gql'
 import { client } from "../libs/client"
 
-function useGetUsersQuery() {
-  return useQuery({
-    queryKey: ['user'],
-    queryFn: async () => await client(graphql('query GetUser($where: UserWhereUniqueInput!) {\n  user(where: $where) {\n    id\n    name\n    email\n  }\n}'), { where: { id: 1 } }),
+const useGetUserQuery = (id: number) =>
+  useQuery({
+    queryKey: ['user', id],
+    queryFn: async () => await client(graphql('query GetUser($where: UserWhereUniqueInput!) {\n  user(where: $where) {\n    id\n    name\n    email\n    accounts {\n      id\n      name\n    }\n  }\n}'), { where: { id } }),
   })
-}
 
-export { useGetUsersQuery }
+export { useGetUserQuery }
